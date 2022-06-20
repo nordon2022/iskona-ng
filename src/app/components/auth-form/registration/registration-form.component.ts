69a8440f38d3be4f52ca1e211e61cb2cbb80d09a
nordon2022 <nordon173@gmail.com>
2022-06-20 22:59:49 +0300

    feat(auth): задел под аутентификацию

--- src/app/components/auth-form/registration/registration-form.component.ts
+++ src/app/components/auth-form/registration/registration-form.component.ts
@@ -1,19 +1,24 @@
 import {Component} from "@angular/core";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reg-form',
   templateUrl: './registration-form.component.html'
 })
 export class RegistrationFormComponent {
-  public form: FormGroup = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
-  });
+  constructor(private fb: FormBuilder) {
+  }
+  public form: FormGroup = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    repeat_password: ['', Validators.required],
+  })
 
   public loading: boolean = false;
 
   public onSubmit(): void {
-
+    console.log(this.form.getRawValue())
   }
 }
